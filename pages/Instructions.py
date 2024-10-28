44d3b051d0565d71d486767fbc2f904fb64cdc42
Bell <namtranquoc95lqd@gmail.com>
2024-10-28 23:26:39 +0700

    Refactor code to fix file path for instructions.html

--- pages/Instructions.py
+++ pages/Instructions.py
@@ -1,16 +1,16 @@
 from __future__ import division
 import streamlit as st
-import os
 from authentication import make_sidebar
+import os
 
 make_sidebar()
 
 st.title("Instructions")
 st.sidebar.success('Select the ticket data or sales data')
 
-# Read the HTML file content
-path = os.path.join(os.path.dirname(__file__), '../instructions.html')
-with open(path, 'r') as file:
+# Read the HTML file content from the parent directory
+html_file_path = os.path.join(os.path.dirname(__file__), '..', 'instructions.html')
+with open(html_file_path, 'r') as file:
     html_content = file.read()
 
 # Display the HTML content
